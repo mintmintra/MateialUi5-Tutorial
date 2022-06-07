5931e4f08ae62e68df8b7141acac91afbdb2393a
mintmintra <mintracss019@gmail.com>
2022-06-07 20:23:10 +0700

    button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from '@mui/material/Button';
+import SettingsIcon from '@mui/icons-material/Settings';
 function App() {
   return (
     <div>
-      Hello
+      <Button variant="text">Text</Button>
+      <Button startIcon={<SettingsIcon />} variant="contained" color="success">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
